faa694311981b4297923c7666708f94093323923
Sasha Goldshtein <goldshtn@gmail.com>
2016-11-30 12:51:09 +0200

    Add functionality, unit test example, and UI test

--- LiveDemoApp/ViewController.swift
+++ LiveDemoApp/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func tapped() {
+        messageLabel.text = "Thank you!"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
